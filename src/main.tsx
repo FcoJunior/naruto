28e6039ea69243decbb5fd2ce58afd7fc43a6066
Jonas <jns.lima@outlook.com>
2022-07-26 22:25:19 -0300

    add menu e buttons

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/home/Home'
 import 'bootstrap/dist/css/bootstrap.css'
+import Menu from './components/item/Menu'
+import Buttons from './components/item/Buttons'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Menu />
+    <Buttons />
     <Home />
   </React.StrictMode>
 )
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,9 @@
 import Item from "../../components/item/Item"
+import './Home.css'
 
 function Home() {
 
-    var list = [1, 2, 3]
+    var list = [1, 2, 3, 4]
 
     return (
         <div className="d-flex">
