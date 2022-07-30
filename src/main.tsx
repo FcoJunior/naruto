54801f2c422500693866f0987bdd4c43311a389c
Francisco Junior <fcojunr@gmail.com>
2022-07-30 15:01:32 -0300

    add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/home/Home'
 import 'bootstrap/dist/css/bootstrap.css'
+import AppRoutes from './routes/Routes'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <AppRoutes />
   </React.StrictMode>
 )
