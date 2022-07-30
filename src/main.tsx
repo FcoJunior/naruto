33309b00f9916392c64c3cfa3ca2f548fe102065
Jonas <jns.lima@outlook.com>
2022-07-30 15:32:42 -0300

    aplicação do css

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/home/Home'
 import 'bootstrap/dist/css/bootstrap.css'
-import Menu from './components/item/Menu'
-import Buttons from './components/item/Buttons'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Menu />
-    <Buttons />
     <Home />
   </React.StrictMode>
 )
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,20 @@
+import Buttons from "../../components/item/Buttons"
 import Item from "../../components/item/Item"
-import './Home.css'
+import Menu from "../../components/item/Menu"
 
 function Home() {
 
     var list = [1, 2, 3, 4]
 
     return (
-        <div className="d-flex">
-            {
-                list.map(x => <Item key={x} />)
-            }
+        <div className="home">
+            <Menu />
+            <Buttons />
+            <div className="d-flex">
+                {
+                    list.map(x => <Item key={x} />)
+                }
+            </div>
         </div>
     )
 }
